5dfe60a7e79b3061a7356d843845b06abaaef752
Maria Gabriela Bermudez <maria.32520312@uru.edu>
2024-10-17 20:29:08 -0400

    git commit -m "segundo commit, modifique algunas cosas para que me funcionara mejor el programa y se guardara la informacion en personas.txt"

--- practica1.cpp
+++ practica1.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <fstream>
-#include <filesystem>
+#include <sys/stat.h>
+#include <string>
 
 using namespace std;
 
 int main(){
-    filesystem::create_directory("data");
-    ofstream archivo("data/personas.txt");
-    if (!archivo){
-        cerr << "Error  creando el archivo"<< endl;
+    struct stat info;
+    if (stat("data", &info) != 0) { 
+        mkdir("data");  
+    }
+     ofstream archivo("data/personas.txt");
+    if (!archivo) {
+        cerr << "Error creando el archivo!" << endl;
         return 1;
     }
+
+    int N;
+    cout<< "ingrese el numero de registros:  ";
+    cin>>  N ;
+    cin.ignore();
+
+    for (int i = 0; i < N; i++)
+    {
+        string nombre, cedula, correo;
+        cout<< "ingrese el nombre:  ";
+        getline (cin, nombre);
+        cout<< "ingrese cedula:";
+        getline(cin, cedula);
+        cout<< "ingrese el correo:  ";
+        getline(cin, correo);
+
+        archivo<< nombre<< "" << cedula << "" << correo<< "\n";
+    }
     archivo.close();
-    cout<< "archivo personas.txt se creo en la carpeta 'data'."<< endl;
+    cout<< " los registros se encuentran en personas.txt "<< endl;
     return 0;
+    
 }
